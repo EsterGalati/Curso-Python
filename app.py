5ed75df1fc407a2bd0b8f26e3ad4c2fb6cfb9c8b
EsterGalati <esterscgalati@gmail.com>
2024-04-11 09:24:57 -0300

    Python

---
+++ app.py
@@ -0,0 +1,85 @@
+'''A ideia do projeto é ser um aplicativo de entrega de comida. Nesse aplicativo, existirão várias funcionalidades, 
+como o cadastro de restaurante e comidas, serviço de entrega, avaliação e tudo isso.Vamos implementar uma dessas funcionalidades, 
+que é exatamente a parte de cadastrar restaurantes.'''
+import os
+
+restaurantes = ['Pizza','Sushi']
+
+
+def exibir_nome_do_programa():
+      print(""" 
+      
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+      """)
+''' fsymbols para estética do código'''
+def exibir_opcoes():
+      print('1. Cadastrar Restaurante')
+      print('2. Listar Restaurante')
+      print('3. Ativar Restaurante')
+      print('4. Sair\n')
+
+def finalizar_app():
+    exibir_subtitulo('Finalizando app')
+
+def voltar_ao_menu_principal():
+    input ()('\nDigite uma tecla para voltar ao menu principal.')
+    main()
+
+def opcao_invalida():
+    print('Opção Inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando Restaurante\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+        
+        if opcao_escolhida == 1: 
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: 
+        opcao_invalida()
+
+# código omitido
+
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__=='__main__':
+    main()
